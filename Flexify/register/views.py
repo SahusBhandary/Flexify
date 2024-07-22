679bfb91fbc5e12c95dc0e71aede3a42d74fa454
Meshane Peiris <meshanepeiris@gmail.com>
2024-07-21 20:07:19 -0400

    trying database.

--- Flexify/register/views.py
+++ Flexify/register/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from .forms import RegisterForm
+from .models import User
 
 # Create your views here.
 def register(response):
     if response.method == "POST":
         form = RegisterForm(response.POST)
         if form.is_valid():
+            new_user = User(username= "ben")
+            new_user.save()
             form.save()
     else:
         form = RegisterForm()
