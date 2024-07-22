b881c6c6dcc1c62da6aac0a7f527d34cf651d260
Meshane Peiris <meshanepeiris@gmail.com>
2024-07-22 14:53:08 -0400

    Changes to view.

--- Flexify/register/views.py
+++ Flexify/register/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .forms import RegisterForm
 from main.models import User
+from django.http import HttpResponseRedirect
 
 # Create your views here.
 def register(response):
