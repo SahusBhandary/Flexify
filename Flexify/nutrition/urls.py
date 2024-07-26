c6c6718e7153b922bba1755fb1b6e0fc75af512b
SahusBhandary <sahusbhandary04@gmail.com>
2024-07-26 15:39:32 -0400

    Fixed links on workout page

--- Flexify/nutrition/urls.py
+++ Flexify/nutrition/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('foodData/', views.foodData),
+    path('foodData/', views.foodData, name='nutrition'),
     path('foodData/nutrition', views.displayNutrition),
     path('foodData/added', views.added),
     path('foodData/removed', views.removed),
--- Flexify/workout/urls.py
+++ Flexify/workout/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('exercise/', views.exercise_API_req, name='exercise_api_req'),
+    path('exercise/', views.exercise_API_req, name='create_workout'),
     path('workout/', views.redirectCreateWorkout),
     path('createWorkout/', views.redirectCreateWorkout),
     path('createWorkout/<str:username>/', views.displayWorkout, name='display_workout'),
